b973ca6706d1943649a69621c3715bb3470cfb27
Noa Fisher <Noa Fisher>
2016-08-03 12:20:23 +0300

    organization

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
 
 var imgArr = document.querySelectorAll('img');
-console.log(imgArr);
 var rightBtn = document.querySelector('.right');
 var leftBtn = document.querySelector('.left');
-
 var imgArrSrc = ['http://www.106fm.co.il/images/cache/170x95/2016-08/f1942e7e59e5b3797e51590301b0bd0b.jpg?1470134269' ,'http://www.106fm.co.il/images/cache/170x95/2016-07/530c26359a40f63ddafbbcdea1e2249a.jpg?1469837557','http://www.106fm.co.il/images/cache/170x95/2016-07/397469cb1c45ba24d47246ccaea159a7.jpg?1469534907','http://www.106fm.co.il/images/cache/170x95/2016-06/655a8ae6d7eda2c1dcb88c0fd244d370.jpg?1466755490','http://www.106fm.co.il/images/cache/170x95/2016-08/f1942e7e59e5b3797e51590301b0bd0b.jpg?1470134269']
 var index=2;
-
+//module
 var slider = (function(){
 
     function switchImgRight (e) {
